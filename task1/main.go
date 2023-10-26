d4b661348e067205ede73b87cb688bcdec8c6021
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-26 19:23:17 +0530

    add comment in the code

--- task1/main.go
+++ task1/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	var Menu [2]string	
-	
-	arr := [5]string{"apple", "oranges", "banana", "papaya", "peach"}
-	
-	Menu[0]="hamburger"
-	Menu[1]="salad"
+	var Menu [2]string	//declare an array 
 
+	arr := [5]string{"apple", "oranges", "banana", "papaya", "peach"} // declare an array with some values
+	
+	Menu[0]="hamburger"	// add values to array Menu at index 0
+	Menu[1]="salad"		/// add values to array Menu at index 1
+
+	// iterate over array menu with for loop
+	// each iteration will print the values inside the array menu
 	for _, item := range Menu{
 		fmt.Printf("List of food item found in menu: %s\n", item)
 		
@@ -17,6 +19,8 @@ func main(){
 
 	fmt.Println()
 
+	// iterate over array arr with for loop
+	// each iteration will print the values inside the arr menu along with the index
 	for index, val := range arr{
 		fmt.Printf("This is %s and its index in the array is %d \n",val, index)
 	}
