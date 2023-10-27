0a1ae97f4d2c25d5d32992fa4b5d4d69b7ba88a3
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-27 11:25:53 +0530

    adding comment in main

--- task2/main.go
+++ task2/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main(){
-	n := rand.Intn(100)
-	fun1(n)
-	fun2(n)
-	fun3(n)
+	n := rand.Intn(100)	//generate a random number between 1-100
+	fun1(n)	// as per point 1 from problem statement 
+	fun2(n)	// as per point 2 from problem statement
+	fun3(n)	// as per point 3 from problem statement
 }
 
 func fun1(num int) {
@@ -33,7 +33,7 @@ func fun2(num int) {
 }
 
 func fun3(num int) {
-	if num > 50 && num%2 == 0{
+	if num > 50 && num%2 == 0{	// if remainder is zero, it is even number 
 		fmt.Println("It's closer to 100, and it's even!")
 	} else if num < 50 {
 		fmt.Println("It's closer to 0")
