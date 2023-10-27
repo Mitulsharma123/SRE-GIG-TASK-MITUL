832c80af85af17971464f4ba684244db90d0b551
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-27 17:10:17 +0530

    code using crypto/rand package

--- task2/main.go
+++ task2/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"math/rand"
+	"crypto/rand"
 	"fmt"
+	"math/big"
 )
 
+
 func main(){
-	n := rand.Intn(100)	//generate a random number between 1-100
-	fun1(n)	// as per point 1 from problem statement 
-	fun2(n)	// as per point 2 from problem statement
-	fun3(n)	// as per point 3 from problem statement
+	var max big.Int
+	max.SetInt64(100)
+	n, err := rand.Int(rand.Reader, &max)	//generate a random number between 1-100
+	if err != nil{
+		panic(err)
+	}
+	random := n.Int64()
+	//fmt.Println(random)
+
+	fun1(random)	// as per point 1 from problem statement 
+	fun2(random)	// as per point 2 from problem statement
+	fun3(random)	// as per point 3 from problem statement
 }
 
-func fun1(num int) {
+func fun1(num int64) {
 	fmt.Printf("Generated random number is: %d\n",num)
 		if num > 50 {
 			fmt.Println("It's closer to 100")
@@ -22,7 +32,7 @@ func fun1(num int) {
 		} 	
 }
 
-func fun2(num int) {
+func fun2(num int64) {
 	fmt.Printf("Generated random number is: %d\n",num)
 		if num == 50 {
 			fmt.Println("It's 50!")
@@ -32,7 +42,7 @@ func fun2(num int) {
 	}
 }
 
-func fun3(num int) {
+func fun3(num int64) {
 	fmt.Printf("Generated random number is: %d\n",num)
 		if num > 50 && num%2 == 0{	// if remainder is zero, it is even number 
 			fmt.Println("It's closer to 100, and it's even!", num)
