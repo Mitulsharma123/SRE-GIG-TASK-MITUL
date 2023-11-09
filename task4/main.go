22c05c613e5e43f30adca277872b6931224931b7
Mitulsharma123 <sharmamitul72@gmail.com>
2023-11-09 10:53:06 +0530

    add task4 details

---
+++ task4/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"task4/todo"
+)
+
+func main(){
+	todo.RootCmd.Execute()
+}
+
---
+++ task4/todo/root.go
@@ -0,0 +1,9 @@
+package todo
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "todo is a cli task manager",
+	}
+  
